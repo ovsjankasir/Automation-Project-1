5ea5f582ac8538361fbdf526c00caa1710c0c06e
ovsjankasir <iljaovsjannikov@gmail.com>
2024-05-09 17:30:01 +0300

    My comments about the commit

--- cypress/e2e/Ilja_Ovsjannikov_registration_form_2_test.cy.js
+++ cypress/e2e/Ilja_Ovsjannikov_registration_form_2_test.cy.js
@@ -10,25 +10,67 @@ describe('Section 1: Functional tests', () => {
 
     it('User can use only same both first and validation passwords', ()=>{
         // Add test steps for filling in only mandatory fields
+        cy.get('[data-testid="phoneNumberTestId"]').type('5656565656')
+        cy.get('#username').type('Something')
+        cy.get('[name="name"]').type('Ilja')
+        cy.get('[name="lastName"]').type('Ovsjannikov')
+        cy.get('[name="password"]').type('Password123')
+        cy.get('[name="email"]').type('email@gmail.com')
         // Type confirmation password which is different from first password
+        cy.get('[name="confirm"]').type('Password12')
         // Assert that submit button is not enabled
+        cy.get('#logo').click()
+        cy.get('.submit_button').should('not.be.enabled')
         // Assert that successful message is not visible
+        cy.get('#success_message').should('not.be.visible')
         // Assert that error message is visible
+        cy.get('.error_message').should('be.visible')
         // Change the test, so the passwords would match
+        cy.get('[name="confirm"]').clear()
+        cy.get('[name="confirm"]').type('Password123')
+        cy.get('#logo').click()
         // Add assertion, that error message is not visible anymore
+        cy.get('.error_message').should('not.be.visible')
         // Add assertion, that submit button is now enabled
+        cy.get('.submit_button').should('be.enabled')
     })
     
     it('User can submit form with all fields added', ()=>{
         // Add test steps for filling in ALL fields
+        cy.get('[data-testid="phoneNumberTestId"]').type('5656565656')
+        cy.get('#username').type('Something')
+        cy.get('[name="name"]').type('Ilja')
+        cy.get('[name="lastName"]').type('Ovsjannikov')
+        cy.get('[name="password"]').type('Password123')
+        cy.get('[name="email"]').type('email@gmail.com')
+        cy.get('[name="confirm"]').type('Password123')
+        cy.get('#cssFavLanguage').check()
+        cy.get('#vehicle2').check()
+        cy.get('#cars').select('opel')
+        cy.get('#animal').select('snake')
         // Assert that submit button is enabled
+        cy.get('.submit_button').should('be.enabled')
         // Assert that after submitting the form system show successful message
+        cy.get('.submit_button').click()
+        cy.get('#success_message').should('be.visible')
+        
     })
 
     it('User can submit form with valid data and only mandatory fields added', ()=>{
         // Add test steps for filling in ONLY mandatory fields
+        cy.get('[data-testid="phoneNumberTestId"]').type('5656565656')
+        cy.get('#username').type('Something')
+        cy.get('[name="name"]').type('Ilja')
+        cy.get('[name="lastName"]').type('Ovsjannikov')
+        cy.get('[name="password"]').type('Password123')
+        cy.get('[name="confirm"]').type('Password123')
+        cy.get('[name="email"]').type('email@gmail.com')
         // Assert that submit button is enabled
+        cy.get('#logo').click()
+        cy.get('.submit_button').should('be.enabled')
         // Assert that after submitting the form system shows successful message
+        cy.get('.submit_button').click()
+        cy.get('#success_message').should('be.visible')
 
         // example, how to use function, which fills in all mandatory data
         // in order to see the content of the function, scroll to the end of the file
@@ -37,7 +79,22 @@ describe('Section 1: Functional tests', () => {
 
     // Add at least 1 test for checking some mandatory field's absence
 
+    it('Verify that the submit button is not enabled when phone number is not present', ()=>{
+        cy.get('#username').type('Something')
+        cy.get('[name="name"]').type('Ilja')
+        cy.get('[name="lastName"]').type('Ovsjannikov')
+        cy.get('[name="password"]').type('Password123')
+        cy.get('[name="confirm"]').type('Password123')
+        cy.get('[name="email"]').type('email@gmail.com')
+        // Assert that submit button is not enabled
+        cy.get('#logo').click()
+        cy.get('.submit_button').should('not.be.enabled')
+
+    })
+
 })
+
+
 
 /*
 Assignement 5: create more visual tests
@@ -55,6 +112,12 @@ describe('Section 2: Visual tests', () => {
 
     it('My test for second picture', () => {
         // Create similar test for checking the second picture
+        cy.log('Will check logo source and size')
+        cy.get('[data-cy="cypress_logo"]').should('have.attr', 'src').should('include', 'cypress_logo')
+        // get element and check its parameter height
+        // it should be less than 178 and greater than 87
+        cy.get('[data-cy="cypress_logo"]').invoke('height').should('be.lessThan', 178)
+            .and('be.greaterThan', 87)   
     });
 
     it('Check navigation part', () => {
@@ -77,6 +140,24 @@ describe('Section 2: Visual tests', () => {
     })
 
     // Create similar test for checking the second link 
+    it('Check navigation part 2', () => {
+        cy.get('nav').children().should('have.length', 2)
+
+        // Get navigation element, find siblings that contains h1 and check if it has Registration form in string
+        cy.get('nav').siblings('h1').should('have.text', 'Registration form number 2')
+        
+        // Get navigation element, find its second child, check the link content and click it
+        cy.get('nav').children().eq(1).should('be.visible')
+            .and('have.attr', 'href', 'registration_form_3.html')
+            .click()
+        
+        // Check that currently opened URL is correct
+        cy.url().should('contain', '/registration_form_3.html')
+        
+        // Go back to previous page
+        cy.go('back')
+        cy.log('Back again in registration form 2')
+    })
 
     it('Check that radio button list is correct', () => {
         // Array of found elements with given selector has 4 elements in total
@@ -101,6 +182,25 @@ describe('Section 2: Visual tests', () => {
     })
 
     // Create test similar to previous one verifying check boxes
+    it('Check that check boxes list is correct', () => {
+        // Array of found elements with given selector has 3 elements in total
+        cy.get('input[type="checkbox"]').should('have.length', 3)
+
+        // Verify labels of the checkboxes
+        cy.get('input[type="checkbox"]').next().eq(0).should('have.text','I have a bike')
+        cy.get('input[type="checkbox"]').next().eq(1).should('have.text','I have a car')
+        cy.get('input[type="checkbox"]').next().eq(2).should('have.text','I have a boat')
+
+        //Verify default state of checkboxes
+        cy.get('input[type="checkbox"]').eq(0).should('not.be.checked')
+        cy.get('input[type="checkbox"]').eq(1).should('not.be.checked')
+        cy.get('input[type="checkbox"]').eq(2).should('not.be.checked')
+
+        // Selecting one will not remove selection from the other checkboxes
+        cy.get('input[type="checkbox"]').eq(0).check()
+        cy.get('input[type="checkbox"]').eq(1).check()
+        cy.get('input[type="checkbox"]').eq(0).should('be.checked')
+    })
 
     it('Car dropdown is correct', () => {
         // Here is just an example how to explicitely create screenshot from the code
@@ -124,6 +224,20 @@ describe('Section 2: Visual tests', () => {
     })
 
     // Create test similar to previous one
+    it('Animal dropdown is correct', () => {
+
+        cy.get('#animal').children().should('have.length', 6)
+        cy.get('#animal').find('option').should('have.length', 6)
+        
+        // Check  that all elements in the dropdown have the correct text
+        cy.get('#animal').find('option').eq(0).should('have.text', 'Dog')
+        cy.get('#animal').find('option').eq(1).should('have.text', 'Cat')
+        cy.get('#animal').find('option').eq(2).should('have.text', 'Snake')
+        cy.get('#animal').find('option').eq(3).should('have.text', 'Hippo')
+        cy.get('#animal').find('option').eq(4).should('have.text', 'Cow')
+        cy.get('#animal').find('option').eq(5).should('have.text', 'Horse')
+        
+    })
 
 })
 
--- cypress/e2e/registration_form_3_test.cy.js
+++ cypress/e2e/registration_form_3_test.cy.js
@@ -6,15 +6,113 @@ beforeEach(() => {
 BONUS TASK: add visual tests for registration form 3
 Task list:
 * Create test suite for visual tests for registration form 3 (describe block)
+
 * Create tests to verify visual parts of the page:
-    * radio buttons and its content
     * dropdown and dependencies between 2 dropdowns:
         * list of cities changes depending on the choice of country
         * if city is already chosen and country is updated, then city choice should be removed
-    * checkboxes, their content and links
-    * email format
  */
 
+describe('Visual tests', () => {
+    it('Check that radio button list is correct', () => {
+        // Array of found elements with given selector has 4 elements in total
+        cy.get('input[type="radio"]').should('have.length', 4)
+
+        // Verify labels of the radio buttons
+        cy.get('input[type="radio"]').next().eq(0).should('have.text','Daily')
+        cy.get('input[type="radio"]').next().eq(1).should('have.text','Weekly')
+        cy.get('input[type="radio"]').next().eq(2).should('have.text','Monthly')
+        cy.get('input[type="radio"]').next().eq(3).should('have.text','Never')
+
+        //Verify default state of radio buttons
+        cy.get('input[type="radio"]').eq(0).should('not.be.checked')
+        cy.get('input[type="radio"]').eq(1).should('not.be.checked')
+        cy.get('input[type="radio"]').eq(2).should('not.be.checked')
+        cy.get('input[type="radio"]').eq(3).should('not.be.checked')
+
+        // Selecting one will remove selection from the other radio button
+        cy.get('input[type="radio"]').eq(0).check().should('be.checked')
+        cy.get('input[type="radio"]').eq(1).check().should('be.checked')
+        cy.get('input[type="radio"]').eq(0).should('not.be.checked')
+    })
+
+    it('Check that check boxes list is correct', () => {
+        // Array of found elements with given selector has 2 elements in total
+        cy.get('input[type="checkbox"]').should('have.length', 2)
+
+        // Verify labels of the checkboxes
+        cy.get('input[type="checkbox"]').next().eq(0).should('have.text','')
+        cy.get('input[type="checkbox"]').next().eq(1).should('have.text','Accept our cookie policy')
+
+        //Verify default state of checkboxes
+        cy.get('input[type="checkbox"]').eq(0).should('not.be.checked')
+        cy.get('input[type="checkbox"]').eq(1).should('not.be.checked')
+
+        // Selecting one will not remove selection from the other checkboxes
+        cy.get('input[type="checkbox"]').eq(0).check()
+        cy.get('input[type="checkbox"]').eq(1).check()
+        cy.get('input[type="checkbox"]').eq(0).should('be.checked')
+
+        cy.get('button').children().eq(0).should('be.visible')
+        .and('have.attr', 'href', 'cookiePolicy.html')
+        .click()
+        cy.url().should('contain', '/cookiePolicy.html')
+        cy.go('back')
+        cy.log('Back again in registration form 3')
+    })
+
+    it('Email field test', () => {
+        const validEmail = 'test@email.com'
+        const invalidEmail = 'test"email.com'
+        // Type valid email and assert that warning message is not visible
+        cy.get('input[name=email]').type(validEmail)
+        cy.get('input[name=email]').should('have.class', 'ng-valid-email')
+        cy.get('#emailAlert').should('not.be.visible')
+        // Type invalid email and assert that warning message is visible
+        cy.get('input[name=email]').clear().type(invalidEmail)
+        cy.get('#emailAlert').should('be.visible')
+
+
+    })
+
+    it('Country dropdown is correct', () => {
+
+        cy.get('#country').children().should('have.length', 4)
+        cy.get('#country').find('option').eq(0).should('have.text', '')
+
+        cy.get('#country').find('option').eq(1).should('have.text', 'Spain')
+        cy.get('#country').select('Spain')
+        cy.get('#city').should('be.visible')
+        cy.get('#city').children().should('have.length', 5)
+        cy.get('#city').find('option').eq(0).should('have.text', '')
+        cy.get('#city').find('option').eq(1).should('have.text', 'Malaga')
+        cy.get('#city').find('option').eq(2).should('have.text', 'Madrid')
+        cy.get('#city').find('option').eq(3).should('have.text', 'Valencia')
+        cy.get('#city').find('option').eq(4).should('have.text', 'Corralejo')
+
+        cy.get('#country').find('option').eq(2).should('have.text', 'Estonia')
+        cy.get('#country').select('Estonia')
+        cy.get('#city').should('be.visible')
+        cy.get('#city').children().should('have.length', 4)
+        cy.get('#city').find('option').eq(0).should('have.text', '')
+        cy.get('#city').find('option').eq(1).should('have.text', 'Tallinn')
+        cy.get('#city').find('option').eq(2).should('have.text', 'Haapsalu')
+        cy.get('#city').find('option').eq(3).should('have.text', 'Tartu')
+
+
+        cy.get('#country').find('option').eq(3).should('have.text', 'Austria')
+        cy.get('#country').select('Austria')
+        cy.get('#city').should('be.visible')
+        cy.get('#city').children().should('have.length', 4)
+        cy.get('#city').find('option').eq(0).should('have.text', '')
+        cy.get('#city').find('option').eq(1).should('have.text', 'Vienna')
+        cy.get('#city').find('option').eq(2).should('have.text', 'Salzburg')
+        cy.get('#city').find('option').eq(3).should('have.text', 'Innsbruck')
+
+        })
+
+
+})
 
 /*
 BONUS TASK: add functional tests for registration form 3
